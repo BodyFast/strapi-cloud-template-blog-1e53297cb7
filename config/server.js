99623aed72ddfdb352a052d4a1a5a81a2d471b54
Matthias Schmeisser <matthiasschmeisser1@gmail.com>
2025-05-20 16:02:48 +0200

    init

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['bQEJ/0EjAX1bOVwnvzYVTkf9ddwiwk+1CPxYX+ZfYAI=', 'ioCPQFVrvfkNmqDaV8ACo0Ltx1/OHbbxbNA1JE4CJhg=']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
